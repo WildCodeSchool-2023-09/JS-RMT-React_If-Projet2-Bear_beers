e10e767eb2692025f1c0b157efcaa7682a76bd9b
BRUART Alexandre <albruart@hotmail.com>
2023-10-26 16:00:54 +0200

    V router 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,24 @@
 import "./App.css";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [beerData, setBeerData] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/api/beer`)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => setBeerData(data))
+      .catch((err) => setError(err));
+  }, []);
+
   const getAllItems = () => {
-    fetch(`${import.meta.env.VITE_BACKEND_URL}/api/items`)
+    fetch(`${import.meta.env.VITE_BACKEND_URL}/api/beer`)
       .then((response) => response.json())
       .then((data) => console.info(data))
       .catch((err) => console.error(err));
@@ -13,6 +29,23 @@ function App() {
       <button type="button" onClick={getAllItems}>
         Get all items
       </button>
+
+      {error ? (
+        <div>Error: {error.message}</div>
+      ) : (
+        <div className="biere-list">
+          {beerData.map((beer, index) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <div className="biere" key={index}>
+              <p>{beer.nom}</p>
+              <img src={beer.img} alt={beer.nom} />
+              <p>{beer.Prix}€</p>
+              <p>{beer.description}</p>
+              <p>{beer.type}</p>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
